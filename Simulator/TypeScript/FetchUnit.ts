1253da14a979218c94385e41c263590c34f64950
Hassan Munir <hassanmunir@live.com>
2013-12-20 22:20:57 +0000

    Dynamic branch prediction

--- Simulator/TypeScript/FetchUnit.ts
+++ Simulator/TypeScript/FetchUnit.ts
@@ -1,8 +1,11 @@
 class FetchUnit {
     public LinkStack: number[];
+    public BranchHistory: number[]; // 0 = "strongly not taken", 1 = "weakly not taken", 2 = "weakly taken", 3 = "strongly taken"
+
 
     constructor() {
         this.LinkStack = [];
+        this.BranchHistory = [];
     }
 
     fetch(allInstructions: Instructions.Instruction[]): Instructions.Instruction[] {
@@ -10,7 +13,8 @@ class FetchUnit {
         var instructions: Instructions.Instruction[] = [];
 
         for (var i = 0; i < numInstructions; i++) {
-            if (_cpu.getProgramCounter() >= allInstructions.length) {
+            var pc = _cpu.getProgramCounter();
+            if (pc >= allInstructions.length) {
                 //If no instructions are left, stop
                 break;
             } else {
@@ -22,20 +26,20 @@ class FetchUnit {
                     _cpu.incrementProgramCounter();
                 else {
 
-                    this.LinkStack.push(_cpu.getProgramCounter());
-                    _cpu.setProgramCounter(this.predictBranch(instruction));
+                    this.LinkStack.push(pc);
+                    _cpu.setProgramCounter(this.predictBranch(instruction, pc));
                 }
             }
         }
+        Display.printArray(this.BranchHistory, "BranchHistory");
         return instructions;
     }
 
-    predictBranch(instruction: Instructions.Instruction): number {
+    predictBranch(instruction: Instructions.Instruction, pc : number): number {
         /// <summary>
         ///     Takes a branch instruction and returns the address of the next instruction that should be fetched
         /// </summary>
         var nextPc: number;
-
 
         if (instruction.type != Enums.ExecutionUnit.BranchUnit)
             throw Error("The instruction is not a branch");
@@ -44,7 +48,20 @@ class FetchUnit {
             //Branch will definitely be taken
             nextPc = +instruction.operands[0];
         } else {
-            nextPc = +instruction.operands[0];
+            var key = pc;
+            if (this.BranchHistory.hasOwnProperty(key.toString())) {
+                
+            } else {
+                //add to the branch history, initialised as "strongly taken"
+                this.BranchHistory[key] = 3;
+            }
+
+            if (this.BranchHistory[key] > 1)
+            // branch taken
+                nextPc = +instruction.operands[0];
+            else {
+                nextPc = pc + 1;
+            }
         }
 
         return nextPc;
@@ -54,11 +71,20 @@ class FetchUnit {
         /// <summary>
         ///     if a branch was not taken, the execution must continue from the line after where the branch was
         /// </summary>
-        _cpu.setProgramCounter(this.LinkStack.pop() + 1);
+        var key = this.LinkStack.pop();
+        _cpu.setProgramCounter(key + 1);
+
+        if (this.BranchHistory[key] != 0)
+            this.BranchHistory[key]--;
     }
 
     branchTaken() {
-        this.LinkStack.shift();
+        var key = this.LinkStack.shift();
+
+        if (this.BranchHistory[key] < 3)
+            this.BranchHistory[key]++;
     }
+
+
 
 }
